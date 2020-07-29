63db09ee089b1c5d6fa154e98d8fb4f6bb57a6c2
Gagan Singh <gagansingh.dev@outlook.com>
2020-07-29 18:42:58 -0400

    Finished Ex 8.8

---
+++ library-frontend/src/queries.js
@@ -0,0 +1,22 @@
+import { gql } from "@apollo/client";
+
+export const ALL_AUTHORS = gql`
+   {
+      allAuthors {
+         name
+         born
+         bookCount
+      }
+   }
+`;
+
+export const ALL_BOOKS = gql`
+   {
+      allBooks {
+         title
+         author
+         published
+         genres
+      }
+   }
+`;
